3bb7291d3a48c9da1453d37e8b4cc3c054e36c4f
rnnxtrem <113645623+rnnxtrem@users.noreply.github.com>
2024-08-04 16:22:36 -0300

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menu = document.getElementById('menu');
-
-    menuToggle.addEventListener('click', function() {
-        menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
-    });
-
-    FUTIBA.init({
-        apiKey: "I2TRxJFpb8wFt9nNNPrk"
-    });
+FUTIBA.init({
+    apiKey: "I2TRxJFpb8wFt9nNNPrk"
 });
